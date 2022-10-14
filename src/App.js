4e4165cc33d76d17b8bbf093f1bdfc6c776e229b
Ramazan Zholdas <61927828+RamazanZholdas@users.noreply.github.com>
2022-10-14 10:41:52 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import VideoEdit from './components/VideoEdit';
+import Main from './components/Main';
+import Title from './components/Title';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Title />
+      <VideoEdit />
+      <Main />
+      <Footer />
     </div>
   );
 }
